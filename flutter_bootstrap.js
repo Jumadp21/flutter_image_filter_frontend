dd48558486443dee4dfc708506810f84ba59a5ad
Juleicy <juleicy.de@lasallistas.org.mx>
2025-11-18 02:41:34 -0500

    Actualizo frontend con build nuevo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1717173344"
+    serviceWorkerVersion: "2471705180"
   }
 });
